fc5ff324721872a8dbfb1fd9e700c2e852419f24
Zimenokk <dzimenokk@gmail.com>
2022-05-16 12:49:52 +0300

    11

--- lab2/app.js
+++ lab2/app.js
@@ -1,3 +1,5 @@
+let firstUplBtn = document.getElementById("firstUplBtn")
+let secondUplBtn = document.getElementById("secondUplBtn")
 let thirdUPL = document.getElementById("thirdUPL")
 let thirdQUE = document.getElementById("thirdQUE")
 
@@ -8,32 +10,63 @@ let secondReadyOutput = document.getElementById("secondReadyOutput")
 let thirdOutput = document.getElementById("thirdOutput")
 let thirdReadyOutput = document.getElementById("thirdReadyOutput")
 
-let basicLink = "https://zimenokk.github.io/oop-labs/lab2/basicQueue.json"
 
-function jsonUPL(link){
-    var requestURL = link;
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
+let basicLink = "https://zimenokk.github.io/oop-labs/lab2/basicQueue.json";
+let priorityLink = "https://zimenokk.github.io/oop-labs/lab2/priorityQueue.json";
+let reverseLink = "";
 
-    request.onload = function() {
-        uploadedQue = request.response;
-        basicQueueAssign(uploadedQue)
-    }
+firstUplBtn.addEventListener('click' ,()=>basicQueueAssign(uploadJson(basicLink)));
+secondUplBtn.addEventListener('click' ,()=>priorityQue(uploadJson(priorityLink)));
+thirdUPL.addEventListener('click', ()=>reverseQue(uploadJson(reverseLink)));
+
+
+
+async function uploadJson(link){
+    var request = await fetch(link);
+    let result = await request.json();
+    // console.log(result)
+    return result
 }
-jsonUPL(basicLink)
-jsonUPL(basicLink)
-jsonUPL(basicLink)
-function basicQueueAssign(jsonObj){ //перша черга
-    let count=0;
-    for (let key in jsonObj){
-        firstOutput.innerHTML +=jsonObj[key];
-        firstOutput.innerHTML +="; ";
-        firstReadyOutput.innerHTML +=jsonObj[key];
-        firstReadyOutput.innerHTML +="; ";
-        // count++
+
+    function basicQueueAssign(jsonObj){
+        let count=0;
+        jsonObj.then(data =>{
+            // console.log(data)
+            for (let key  in data){
+                firstOutput.innerHTML +=data[key];
+                firstOutput.innerHTML +="; ";
+                firstReadyOutput.innerHTML +=data[key];
+                firstReadyOutput.innerHTML +="; ";
+                // count++
+            }
+        })
     }
-}
+    function priorityQue(jsonObj){
+        jsonObj.then(data =>{
+            // console.log(data)
+            let jsonKeys = Object.keys(data);
+             jsonKeys.forEach(key=>{
+
+                 secondReadyOutput.innerHTML +=data[key];
+                 secondReadyOutput.innerHTML +="; ";
+
+             })
+        })
+    }
+
+    function reverseQue(jsonObj){
+        jsonObj.then(data =>{
+            // console.log(data)
+            let jsonKeys = Object.keys(data);
+            jsonKeys.forEach(key=>{
+
+                secondReadyOutput.innerHTML +=data[key];
+                secondReadyOutput.innerHTML +="; ";
+
+            })
+        })
+    }
+
+
 
 
