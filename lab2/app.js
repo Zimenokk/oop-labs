c84f2b13cd0f1d8b7bf27263037e4eef8e56ce84
Zimenokk <dzimenokk@gmail.com>
2022-05-16 03:38:42 +0300

    11

--- lab2/app.js
+++ lab2/app.js
@@ -1,12 +1,39 @@
-let firstIN = document.getElementById("firstIN")
-let firstBTN = document.getElementById("firstBTN")
-let secondIN = document.getElementById("secondIN")
-let secondBTN = document.getElementById("secondBTN")
-let firstOutput = document.getElementsByClassName("firstOutput")
-let secondOutput = document.getElementsByClassName("secondOutput")
-let thirdOutput = document.getElementsByClassName("thirdOutput")
+let thirdUPL = document.getElementById("thirdUPL")
+let thirdQUE = document.getElementById("thirdQUE")
 
-firstBTN.addEventListener('click', function (){
+let firstOutput = document.getElementById("firstOutput")
+let firstReadyOutput = document.getElementById("firstReadyOutput")
+let secondOutput = document.getElementById("secondOutput")
+let secondReadyOutput = document.getElementById("secondReadyOutput")
+let thirdOutput = document.getElementById("thirdOutput")
+let thirdReadyOutput = document.getElementById("thirdReadyOutput")
+
+let basicLink = "https://zimenokk.github.io/oop-labs/lab2/basicQueue.json"
+
+function jsonUPL(link){
+    var requestURL = link;
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        uploadedQue = request.response;
+        basicQueueAssign(uploadedQue)
+    }
+}
+jsonUPL(basicLink)
+jsonUPL(basicLink)
+jsonUPL(basicLink)
+function basicQueueAssign(jsonObj){ //перша черга
+    let count=0;
+    for (let key in jsonObj){
+        firstOutput.innerHTML +=jsonObj[key];
+        firstOutput.innerHTML +="; ";
+        firstReadyOutput.innerHTML +=jsonObj[key];
+        firstReadyOutput.innerHTML +="; ";
+        // count++
+    }
+}
 
 
-})
